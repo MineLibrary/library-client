5c640ee32917316c90d8a2fcf096fe7659161b8d
Javid Sadigli <cavidoffical@gmail.com>
2025-01-20 17:43:46 +0400

    Started Library Client

---
+++ src/main/java/az/atlacademy/libraryclient/LibraryclientApplication.java
@@ -0,0 +1,13 @@
+package az.atlacademy.libraryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/atlacademy/libraryclient/LibraryclientApplicationTests.java
@@ -0,0 +1,13 @@
+package az.atlacademy.libraryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
